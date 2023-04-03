83d255119e097b323e3c0516e6219a5e7ec29a9c
DealMaker <support@dealmaker.tech>
2023-04-03 21:50:52 +0000

    Update SDKs

--- v1/clients/javascript/src/model/CreateCompanyRequest.js
+++ v1/clients/javascript/src/model/CreateCompanyRequest.js
@@ -16,7 +16,7 @@ import ApiClient from '../ApiClient';
 /**
  * The CreateCompanyRequest model module.
  * @module model/CreateCompanyRequest
- * @version 0.75.0
+ * @version 0.75.1
  */
 class CreateCompanyRequest {
     /**
--- v1/clients/javascript/src/model/V1EntitiesDealSetup.js
+++ v1/clients/javascript/src/model/V1EntitiesDealSetup.js
@@ -17,7 +17,7 @@ import V1EntitiesDealSetupUser from './V1EntitiesDealSetupUser';
 /**
  * The V1EntitiesDealSetup model module.
  * @module model/V1EntitiesDealSetup
- * @version 0.75.0
+ * @version 0.75.1
  */
 class V1EntitiesDealSetup {
     /**
